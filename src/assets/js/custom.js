dd8191cfcdef90d22636a504f929e4fd6f32eae2
Jhon Altamirano <jaltamirano@in-planet.org>
2021-10-29 12:10:18 -0500

    Fin seccion 8

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -159,4 +159,53 @@ const customInitFunctions = () =>{
     });
 }
 
+$(function() {
+    var set = function() {
+        var width = (window.innerWidth > 0) ? window.innerWidth : this.screen.width;
+        var topOffset = 0;
+        if (width < 1170) {
+            $("body").addClass("mini-sidebar");
+            $('.navbar-brand span').hide();
+            $(".sidebartoggler i").addClass("ti-menu");
+        } else {
+            $("body").removeClass("mini-sidebar");
+            $('.navbar-brand span').show();
+        }
+
+        var height = ((window.innerHeight > 0) ? window.innerHeight : this.screen.height) - 1;
+        height = height - topOffset;
+        if (height < 1) height = 1;
+        if (height > topOffset) {
+            $(".page-wrapper").css("min-height", (height) + "px");
+        }
+
+    };
+    $(window).ready(set);
+    $(window).on("resize", set);
+
+    // ============================================================== 
+    // Theme options
+    // ==============================================================     
+    $(".sidebartoggler").on('click', function() {
+        if ($("body").hasClass("mini-sidebar")) {
+            $("body").trigger("resize");
+            $("body").removeClass("mini-sidebar");
+            $('.navbar-brand span').show();
+            
+        } else {
+            $("body").trigger("resize");
+            $("body").addClass("mini-sidebar");
+            $('.navbar-brand span').hide();
+            
+        }
+    });
+
+    // this is for close icon when navigation open in mobile view
+    $(".nav-toggler").click(function() {
+        $("body").toggleClass("show-sidebar");
+        $(".nav-toggler i").toggleClass("ti-menu");
+        $(".nav-toggler i").addClass("ti-close");
+    });
+})
+
 customInitFunctions();
